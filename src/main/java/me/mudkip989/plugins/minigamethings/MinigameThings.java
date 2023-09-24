aea8e329a1d4fdf2a556ebf2bbf83a8ba5173573
mudkip989 <muddcop@gmail.com>
2023-09-23 20:25:00 -0500

    push to git 2

--- src/main/java/me/mudkip989/plugins/minigamethings/MinigameThings.java
+++ src/main/java/me/mudkip989/plugins/minigamethings/MinigameThings.java
@@ -1,11 +1,21 @@
 package me.mudkip989.plugins.minigamethings;
 
+import org.bukkit.plugin.*;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.awt.desktop.*;
+import java.util.logging.*;
 
 public final class MinigameThings extends JavaPlugin {
 
+    private static Plugin instance;
+
+    public static Logger log = Logger.getLogger("Minigame.exe");
+
     @Override
     public void onEnable() {
+        instance = this;
+        log.info( "Getting config...");
         // Plugin startup logic
 
     }
